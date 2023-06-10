ee8c1870a17c37036c0e536a14173f6b0bad07e1
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-11 08:31:43 +0900

    ✨ shadcn/uiを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import "./globals.css";
 
 import { Inter } from "next/font/google";
 
+import { Toaster } from "../components/ui/toaster";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
     default: siteConfig.title,
-    template: `%s - ${siteConfig.title}}`,
+    template: `%s - ${siteConfig.title}`,
   },
   description: siteConfig.description,
 };
@@ -17,7 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
