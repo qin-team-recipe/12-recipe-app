fcc66dc38245f27b23a75a56d5670a398458460f
shun1121 <hikitanikun.ware80000@gmail.com>
2023-06-14 00:22:01 +0900

    :sparkles: シェフ名の表示修正
    シェフ名の表示修正

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,5 +1,24 @@
+import { ChefIcon } from "@/src/components/chef-icon";
+
 const page = () => {
-  return <div>favorite</div>;
+  return (
+    <div>
+      <div>favorite</div>
+      <div className="flex">
+        {/* map処理をして横スクロールできるようにする */}
+        <ChefIcon
+          routingUrl="/"
+          imageUrl="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+          chefName="山田シェフ山田シェフ山田シェフ"
+        />
+        <ChefIcon
+          routingUrl="/"
+          imageUrl="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+          chefName="山田シェフ山田シェフ山田シェフ"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/components/chef-icon.tsx
+++ src/components/chef-icon.tsx
@@ -6,13 +6,14 @@ type Props = {
   chefName: string;
 };
 
+// お気に入りシェフアイコン
 export const ChefIcon = ({ routingUrl, imageUrl, chefName }: Props) => {
   return (
-    <div className="relative">
+    <div className="relative w-[68px] h-[87px]">
       <Link href={routingUrl} className="inline-block rounded-full h-[68px] w-[68px] overflow-hidden">
         <img src={imageUrl} alt="Icon" className="object-cover w-full h-full" />
       </Link>
-      <p className="text-[12px] text-black absolute left-[4px] bottom-[-12px]">{chefName}</p>
+      <p className="text-[12px] text-black absolute left-[4px] bottom-[-12px] line-clamp-2 leading-[14px]">{chefName}</p>
     </div>
   );
 };
