c2d3e194e944063f2dc5fa5fee12d30e294fdde9
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-17 23:55:02 +0900

    ♻️ routesの改良

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,13 +1,19 @@
 import { Heart, Search, ShoppingCart } from "lucide-react";
 
+export const searchBasePath = "/search";
+
+export const favoriteBasePath = "/favorite";
+
+export const shoppingBasePath = "/shopping";
+
 export const sideBarRoutes = (pathname: string) => [
-  { href: "/", label: "話題を検索", icon: Search, active: pathname === "/" },
-  { href: "/favorite", label: "お気に入り", icon: Heart, active: pathname === "/favorite" },
-  { href: "/shopping", label: "買い物リスト", icon: ShoppingCart, active: pathname === "/shopping" },
+  { href: "/", label: "話題を検索", icon: Search, active: pathname === "/" || pathname.includes(searchBasePath) },
+  { href: favoriteBasePath, label: "お気に入り", icon: Heart, active: pathname === favoriteBasePath },
+  { href: shoppingBasePath, label: "買い物リスト", icon: ShoppingCart, active: pathname === shoppingBasePath },
 ];
 
 export const bottomBarRoutes = (pathname: string) => [
-  { href: "/", label: "検索", icon: Search, active: pathname === "/" },
-  { href: "/favorite", label: "お気に入り", icon: Heart, active: pathname === "/favorite" },
-  { href: "/shopping", label: "お買い物", icon: ShoppingCart, active: pathname === "/shopping" },
+  { href: "/", label: "検索", icon: Search, active: pathname === "/" || pathname.includes(searchBasePath) },
+  { href: "/favorite", label: "お気に入り", icon: Heart, active: pathname === favoriteBasePath },
+  { href: "/shopping", label: "お買い物", icon: ShoppingCart, active: pathname === shoppingBasePath },
 ];
