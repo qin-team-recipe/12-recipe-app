d2a294826a3efd68cf3708f0f223814fcf5912bc
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-17 20:24:46 +0900

    🚚 components/layout に移動

--- src/components/layout/navbar-item.tsx
+++ src/components/layout/navbar-item.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { LucideIcon } from "lucide-react";
 
-import { cn } from "../lib/utils";
+import { cn } from "../../lib/utils";
 
 type Props = {
   icon: LucideIcon;
