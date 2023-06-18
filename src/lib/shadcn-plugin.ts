ed5a4acc60f8759cc3aaf14b92b0a256ba20b359
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-18 17:38:20 +0900

    ♻️ Tailwind CSSの設定周りのリファクタ

---
+++ src/lib/shadcn-plugin.ts
@@ -0,0 +1,137 @@
+import plugin from "tailwindcss/plugin";
+
+export const shadcnPlugin = plugin(
+  function ({ addBase, theme }) {
+    addBase({
+      ":root": {
+        "--background": "0 0% 100%",
+        "--foreground": "222.2 47.4% 11.2%",
+        "--muted": "210 40% 96.1%",
+        "--muted-foreground": "215.4 16.3% 46.9%",
+        "--popover": "0 0% 100%",
+        "--popover-foreground": "222.2 47.4% 11.2%",
+        "--card": "0 0% 100%",
+        "--card-foreground": "222.2 47.4% 11.2%",
+        "--border": "300 4% 89%",
+        "--input": "214.3 31.8% 91.4%",
+        "--primary": "222.2 47.4% 11.2%",
+        "--primary-foreground": "210 40% 98%",
+        "--secondary": "210 40% 96.1%",
+        "--secondary-foreground": "222.2 47.4% 11.2%",
+        "--accent": "0.03 78% 54%",
+        "--accent-foreground": "222.2 47.4% 11.2%",
+        "--destructive": "0 100% 50%",
+        "--destructive-foreground": "210 40% 98%",
+        "--ring": "17 50% 70%",
+        "--radius": "0.5rem",
+      },
+      ".dark": {
+        "--background": "224 71% 4%",
+        "--foreground": "213 31% 91%",
+        "--muted": "223 47% 11%",
+        "--muted-foreground": "215.4 16.3% 56.9%",
+        "--popover": "224 71% 4%",
+        "--popover-foreground": "215 20.2% 65.1%",
+        "--card": "224 71% 4%",
+        "--card-foreground": "213 31% 91%",
+        "--border": "300 4% 69%",
+        "--input": "216 34% 17%",
+        "--primary": "210 40% 98%",
+        "--primary-foreground": "222.2 47.4% 1.2%",
+        "--secondary": "222.2 47.4% 11.2%",
+        "--secondary-foreground": "210 40% 98%",
+        "--accent": "0.03 78% 74%",
+        "--accent-foreground": "210 40% 98%",
+        "--destructive": "0 63% 31%",
+        "--destructive-foreground": "210 40% 98%",
+        "--ring": "17 60% 50%",
+        "--radius": "0.5rem",
+      },
+    });
+
+    addBase({
+      "*": {
+        "@apply border-border": {},
+      },
+      body: {
+        "@apply bg-background text-foreground": {},
+        "font-feature-settings": '"rlig" 1, "calt" 1',
+      },
+    });
+  },
+  {
+    darkMode: ["class"],
+    theme: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      extend: {
+        boxShadow: {
+          "extend-y-top": "0 -4px 6px -1px rgba(0, 0, 0, 0.1),0 -2px 4px -1px rgba(0, 0, 0, 0.04)",
+        },
+        colors: {
+          border: "hsl(var(--border))",
+          input: "hsl(var(--input))",
+          ring: "hsl(var(--ring))",
+          background: "hsl(var(--background))",
+          foreground: "hsl(var(--foreground))",
+          primary: {
+            DEFAULT: "hsl(var(--primary))",
+            foreground: "hsl(var(--primary-foreground))",
+          },
+          secondary: {
+            DEFAULT: "hsl(var(--secondary))",
+            foreground: "hsl(var(--secondary-foreground))",
+          },
+          destructive: {
+            DEFAULT: "hsl(var(--destructive))",
+            foreground: "hsl(var(--destructive-foreground))",
+          },
+          muted: {
+            DEFAULT: "hsl(var(--muted))",
+            foreground: "hsl(var(--muted-foreground))",
+          },
+          accent: {
+            DEFAULT: "hsl(var(--accent))",
+            foreground: "hsl(var(--accent-foreground))",
+          },
+          popover: {
+            DEFAULT: "hsl(var(--popover))",
+            foreground: "hsl(var(--popover-foreground))",
+          },
+          card: {
+            DEFAULT: "hsl(var(--card))",
+            foreground: "hsl(var(--card-foreground))",
+          },
+        },
+        borderRadius: {
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+        keyframes: {
+          "accordion-down": {
+            from: { height: "0" },
+            to: { height: "var(--radix-accordion-content-height)" },
+          },
+          "accordion-up": {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: "0" },
+          },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
+        fontSize: {
+          "extend-ss": "0.625rem",
+        },
+      },
+    },
+    plugins: [],
+  }
+);
