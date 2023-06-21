16fa0f90210385e2acdc532e72372261b8d8565b
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-21 12:43:28 +0900

    ✨ migrate-and-gen実行

--- src/types/SupabaseTypes.ts
+++ src/types/SupabaseTypes.ts
@@ -36,32 +36,177 @@ export interface Database {
         };
         Relationships: [];
       };
+      ChefLink: {
+        Row: {
+          chef_id: string;
+          createdAt: string;
+          id: number;
+          updatedAt: string;
+          url: string;
+        };
+        Insert: {
+          chef_id: string;
+          createdAt?: string;
+          id?: number;
+          updatedAt: string;
+          url: string;
+        };
+        Update: {
+          chef_id?: string;
+          createdAt?: string;
+          id?: number;
+          updatedAt?: string;
+          url?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "ChefLink_chef_id_fkey";
+            columns: ["chef_id"];
+            referencedRelation: "User";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      Favorite: {
+        Row: {
+          createdAt: string;
+          id: number;
+          recipe_id: number;
+          updatedAt: string;
+          user_id: string;
+        };
+        Insert: {
+          createdAt?: string;
+          id?: number;
+          recipe_id: number;
+          updatedAt: string;
+          user_id: string;
+        };
+        Update: {
+          createdAt?: string;
+          id?: number;
+          recipe_id?: number;
+          updatedAt?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "Favorite_recipe_id_fkey";
+            columns: ["recipe_id"];
+            referencedRelation: "Recipe";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "Favorite_user_id_fkey";
+            columns: ["user_id"];
+            referencedRelation: "User";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      Recipe: {
+        Row: {
+          createdAt: string;
+          description: string;
+          id: number;
+          servingCount: number;
+          title: string;
+          updatedAt: string;
+          user_id: string;
+        };
+        Insert: {
+          createdAt?: string;
+          description: string;
+          id?: number;
+          servingCount: number;
+          title: string;
+          updatedAt: string;
+          user_id: string;
+        };
+        Update: {
+          createdAt?: string;
+          description?: string;
+          id?: number;
+          servingCount?: number;
+          title?: string;
+          updatedAt?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "Recipe_user_id_fkey";
+            columns: ["user_id"];
+            referencedRelation: "User";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
       User: {
         Row: {
           createdAt: string;
-          email: string;
-          id: number;
+          id: string;
           name: string;
-          password: string;
+          profile: string | null;
+          profileImage: string | null;
+          role: Database["public"]["Enums"]["RoleType"];
           updatedAt: string;
         };
         Insert: {
           createdAt?: string;
-          email: string;
-          id?: number;
+          id: string;
           name: string;
-          password: string;
+          profile?: string | null;
+          profileImage?: string | null;
+          role?: Database["public"]["Enums"]["RoleType"];
           updatedAt: string;
         };
         Update: {
           createdAt?: string;
-          email?: string;
-          id?: number;
+          id?: string;
           name?: string;
-          password?: string;
+          profile?: string | null;
+          profileImage?: string | null;
+          role?: Database["public"]["Enums"]["RoleType"];
           updatedAt?: string;
         };
         Relationships: [];
+      };
+      UserFollower: {
+        Row: {
+          createdAt: string;
+          followed_id: string;
+          follower_id: string;
+          id: number;
+          updatedAt: string;
+        };
+        Insert: {
+          createdAt?: string;
+          followed_id: string;
+          follower_id: string;
+          id?: number;
+          updatedAt: string;
+        };
+        Update: {
+          createdAt?: string;
+          followed_id?: string;
+          follower_id?: string;
+          id?: number;
+          updatedAt?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "UserFollower_followed_id_fkey";
+            columns: ["followed_id"];
+            referencedRelation: "User";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "UserFollower_follower_id_fkey";
+            columns: ["follower_id"];
+            referencedRelation: "User";
+            referencedColumns: ["id"];
+          }
+        ];
       };
     };
     Views: {
@@ -71,7 +216,7 @@ export interface Database {
       [_ in never]: never;
     };
     Enums: {
-      [_ in never]: never;
+      RoleType: "ADMIN" | "USER" | "CHEF";
     };
     CompositeTypes: {
       [_ in never]: never;
