1fb49be1529cf39a9a61c79b9abeaf463ba1430a
shun1121 <hikitanikun.ware80000@gmail.com>
2023-06-21 20:29:08 +0900

    :recycle: お気に入りチェフリストリファクタ
    お気に入りチェフリストリファクタ

--- src/components/favorite-chef-list.tsx
+++ src/components/favorite-chef-list.tsx
@@ -1,10 +1,10 @@
-import { favChefList } from "../constants/favorite-chef-list";
+import { favChefList } from "../constants/dummy";
 import { ChefIcon } from "./chef-icon";
 
 export const FavoriteChefList = () => {
   return (
     <div>
-      <div className="text-[20px] font-bold ml-[12px] my-[8px]">シェフ</div>
+      <div className="text-[20px] font-bold ml-3 my-[8px]">シェフ</div>
       <div className="flex ml-[12px] gap-[12px] overflow-scroll">
         {favChefList.map((favChef) => (
           <ChefIcon
