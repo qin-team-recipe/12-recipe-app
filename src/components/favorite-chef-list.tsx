f47fab1e82c934ee13a9bfd650c04e2230311e5a
shun1121 <hikitanikun.ware80000@gmail.com>
2023-06-22 19:38:12 +0900

    :recycle: お気に入りシェフアイコンのスタイル修正
    お気に入りシェフアイコンのスタイル修正

--- src/components/favorite-chef-list.tsx
+++ src/components/favorite-chef-list.tsx
@@ -4,8 +4,8 @@ import { ChefIcon } from "./chef-icon";
 export const FavoriteChefList = () => {
   return (
     <div>
-      <div className="text-[20px] font-bold ml-3 my-[8px]">シェフ</div>
-      <div className="flex ml-[12px] gap-[12px] overflow-scroll">
+      <div className="text-xl font-bold ml-3 my-3 text-mauve12">シェフ</div>
+      <div className="flex ml-3 gap-3 overflow-scroll">
         {favChefList.map((favChef) => (
           <ChefIcon
             routingUrl={favChef.routingUrl}
