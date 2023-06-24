a52417eda662376484146a61a05b0f6d555cf84e
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-24 21:31:15 +0900

    ♻️ 既存コンポーネントのリファクタ

--- src/components/layout/top-bar.tsx
+++ src/components/layout/top-bar.tsx
@@ -12,10 +12,26 @@ const TopBar = ({
   trailingComponent: TrailingComponent,
 }: Props) => {
   return (
-    <div className="w-full h-12 flex px-2 md:px-4 items-center border-b border-mauve6">
-      <div className="flex items-center justify-start flex-1">{LeadingComponent}</div>
-      {CenterComponent && <div className="flex items-center justify-center flex-auto mx-auto">{CenterComponent}</div>}
-      <div className="flex items-center justify-end flex-1">{TrailingComponent}</div>
+    <div className="w-full h-16 flex px-2 md:px-4 items-center border-b border-mauve6">
+      {LeadingComponent ? (
+        <div className="flex items-center justify-start flex-1">{LeadingComponent}</div>
+      ) : (
+        <div className="flex-1"></div>
+      )}
+      <div
+        className={
+          LeadingComponent || TrailingComponent
+            ? "flex items-center justify-center flex-auto mx-auto"
+            : "w-full flex items-center justify-center"
+        }
+      >
+        {CenterComponent}
+      </div>
+      {TrailingComponent ? (
+        <div className="flex items-center justify-end flex-1">{TrailingComponent}</div>
+      ) : (
+        <div className="flex-1"></div>
+      )}
     </div>
   );
 };
