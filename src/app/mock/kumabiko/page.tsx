496fef4411c1f3993b416132cb868be3c496a4b1
kumabiko <nousbear.dev@gmail.com>
2023-06-25 23:14:17 +0900

    ✨ シェフ一覧取得API実装

---
+++ src/app/mock/kumabiko/page.tsx
@@ -0,0 +1,10 @@
+import getChefs from "@/src/actions/getChefs";
+
+const page = async () => {
+  const chefs = await getChefs();
+  console.log(chefs);
+
+  return <div className="pt-4">テスト</div>;
+};
+
+export default page;
