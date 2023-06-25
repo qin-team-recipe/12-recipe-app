5e215da9bd4edb3c812438e47b9f1954b9996898
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2023-06-25 22:48:56 +0900

    ✨ レシピタブ押下時のUI作成

---
+++ src/constants/dummy/recipe-card-lists.ts
@@ -0,0 +1,37 @@
+export const recipeCardLists = [
+  {
+    imageUrl: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb',
+    recipeName: 'レシピ名',
+    comment: 'コメント',
+    recipeId: 1,
+    favorites: 1234,
+  },
+  {
+    imageUrl: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb',
+    recipeName: 'レシピ名',
+    comment: 'コメント',
+    recipeId: 1,
+    favorites: 1234,
+  },
+  {
+    imageUrl: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb',
+    recipeName: 'レシピ名',
+    comment: 'コメント',
+    recipeId: 1,
+    favorites: 1234,
+  },
+  {
+    imageUrl: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb',
+    recipeName: 'レシピ名',
+    comment: 'コメント',
+    recipeId: 1,
+    favorites: 1234,
+  },
+  {
+    imageUrl: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb',
+    recipeName: 'レシピ名',
+    comment: 'コメント',
+    recipeId: 1,
+    favorites: 1234,
+  },
+]
