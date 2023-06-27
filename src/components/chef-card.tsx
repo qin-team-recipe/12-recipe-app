0a4936f295b9622c5690e572d0bee8ce44d6822a
YuseiMorimoto <Yusei0509@icloud.com>
2023-06-27 22:36:05 +0900

    test

--- src/components/chef-card.tsx
+++ src/components/chef-card.tsx
@@ -10,6 +10,7 @@ type Props = {
 export const ChefCard = ({ routingUrl, imageUrl, imageText }: Props) => {
   return (
     <div className="w-36 h-56 relative">
+      <div>test</div>
       <Link href={routingUrl}>
         <img className="w-full h-full rounded-2xl" src={imageUrl} alt="chef" />
         <p className="text-xl text-slate-100 font-semibold absolute left-3 bottom-3">{imageText}</p>
