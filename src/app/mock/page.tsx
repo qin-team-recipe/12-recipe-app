bc04291549414ad6978c62005ea53dc233f1c3b1
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-06-28 08:51:55 +0900

    ♻️ mockのルートパスの見直し

--- src/app/mock/page.tsx
+++ src/app/mock/page.tsx
@@ -1,34 +1,26 @@
+import Link from "next/link";
+
 import getAuthenticatedUser from "@/src/actions/getAuthenticatedUser";
-import getRecipes from "@/src/actions/getRecipes";
-import RecipeCard from "@/src/components/recipe-card";
+import { Button } from "@/src/components/ui/button";
 import { Separator } from "@/src/components/ui/separator";
 
-import NewRecipe from "./_components/new-recipe";
-
 const page = async () => {
-  const myRecipes = await getRecipes();
-
   const user = await getAuthenticatedUser();
 
   return (
     <div className="pt-4">
       <h4 className="text-xl font-medium leading-none">🙌 Hello {user?.name} 🙌</h4>
-      <Separator className="my-2" />
-      <NewRecipe />
-      <Separator className="my-2" />
-      <h2 className="pt-2 text-2xl font-extrabold">レシピ一覧</h2>
-      <div className="grid grid-cols-2 gap-4 pt-4">
-        {myRecipes?.map((recipe) => (
-          <div key={recipe.id}>
-            <RecipeCard
-              favorites={recipe.likes.length}
-              comment={recipe.description}
-              recipeName={recipe.title}
-              imageUrl="https://images.unsplash.com/photo-1595295333158-4742f28fbd85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&q=80"
-              recipeId={recipe.id}
-            />
-          </div>
-        ))}
+      <Separator className="my-4" />
+      <div className="flex gap-4">
+        <Link href="/mock/kumabiko" className="flex items-center gap-2">
+          <Button variant={"outline"}>kumabiko</Button>
+        </Link>
+        <Link href="/mock/kame" className="flex items-center gap-2">
+          <Button variant={"outline"}>kame</Button>
+        </Link>
+        <Link href="/mock/tsuboi" className="flex items-center gap-2">
+          <Button variant={"outline"}>tsuboi</Button>
+        </Link>
       </div>
     </div>
   );
