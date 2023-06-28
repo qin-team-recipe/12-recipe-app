9222a0fcf6cdd75eed472facb7904329d85c36da
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2023-06-28 22:02:56 +0900

    ♻️ prettierを効かせた

--- src/app/chef/[id]/page.tsx
+++ src/app/chef/[id]/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 import RecipeCard from "@/src/components/recipe-card";
 import { recipeLists } from "@/src/constants/dummy/recipe-lists";
 
 const page = () => {
   return (
-    <div className="grid grid-cols-2 p-4 gap-4">
+    <div className="grid grid-cols-2 gap-4 p-4">
       {recipeLists.map((list) => (
         <RecipeCard
           imageUrl={list.imageUrl}
