bd11af13bce9b6c652f826d58b2b36994f1a951f
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-07-01 19:02:23 +0900

    ✨ フォロー&アンフォローAPI実装

--- src/components/toggle-button.tsx
+++ src/components/toggle-button.tsx
@@ -5,15 +5,16 @@ type Props = {
   isActive: boolean;
   activeLabel: string;
   inactiveLabel: string;
-  // handleClick: () => void; // フォローやお気に入りをする/外すのクリックイベントを渡す(一旦コメントアウト)
+  formAction: ((formData: FormData) => void) | undefined;
 };
 
-const ToggleButton = ({ isActive, activeLabel, inactiveLabel }: Props) => {
+const ToggleButton = ({ isActive, activeLabel, inactiveLabel, formAction }: Props) => {
   return (
     <>
       <Button
         variant={isActive ? "outline" : "destructive"}
         className={cn("w-fit", "hover:shadow", isActive ? "border-accent bg-mauve1 text-tomato9" : "bg-tomato9")}
+        formAction={formAction}
       >
         {isActive ? activeLabel : inactiveLabel}
       </Button>
