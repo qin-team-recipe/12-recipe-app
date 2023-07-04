5925a5c02fd101df0fa8e5335acd219f43d21a61
shun1121 <hikitanikun.ware80000@gmail.com>
2023-07-04 14:48:36 +0900

    :sparkles: 買い物リストアイテム作成
    買い物リストアイテム作成

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -7,26 +7,11 @@ import { Checkbox } from "../../components/ui/checkbox";
 import { cn } from "../../lib/utils";
 
 const page = () => {
-  const [checked, setChecked] = useState(false);
-  const handleCheckboxClick = () => {
-    setChecked(!checked);
-  }
 
   return (
     <>
       <TopBar centerComponent={<div className="md:text-xl font-bold text-mauve12">買い物リスト</div>} />
-      <ShoppingListItem isChecked={checked} onChange={handleCheckboxClick} />
-
-      {/* shopping-list.tsxの内容 */}
-      <div className="flex items-start gap-x-2 py-2 border-y px-2">
-        <Checkbox
-          className={cn(`rounded-full border-2 border-tomato9 h-5 w-5`)}
-          isChecked={checked}
-          onChange={handleCheckboxClick}
-        />
-        <div className={cn(`flex-1 leading-snug text-mauve12`, checked && "text-mauve8")}>ああ</div>
-        {checked && <button className="text-tomato9">削除</button>}
-      </div>
+      <ShoppingListItem />
     </>
   );
 };
--- src/components/shopping-list.tsx
+++ src/components/shopping-list.tsx
@@ -1,18 +1,36 @@
-import { cn } from "../lib/utils";
-import { Checkbox } from "./ui/checkbox";
+"use client";
 
-type Props = {
-  isChecked?: boolean;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-};
+import { useState } from "react";
+import { Check } from "lucide-react";
+import { cn } from "../lib/utils";
 
 // 買い物リスト
-export const ShoppingListItem = ({ isChecked, onChange }: Props) => {
+export const ShoppingListItem = () => {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <div className="flex items-start gap-x-2 py-2 border-y px-2">
-      <Checkbox className={cn(`rounded-full border-2 border-tomato9 h-5 w-5`)} isChecked={isChecked} onChange={onChange} />
-      <div className={cn(`flex-1 leading-snug text-mauve12`, isChecked && "text-mauve8")}>ああ</div>
-      {isChecked && <button className="text-tomato9">削除</button>}
+    <div className="flex justify-between gap-x-2 border-y pl-4 pr-3 py-4">
+      <div className="flex items-center gap-2">
+        {checked ? (
+          <button
+            aria-label="Toggle checkbox"
+            className="flex h-5 w-5 items-center justify-center rounded-full border-mauve1 bg-mauve8 p-1 text-mauve1"
+            onClick={() => setChecked(false)}
+          >
+            <Check className="h-5 w-5" />
+          </button>
+        ) : (
+          <button
+            aria-label="Toggle checkbox"
+            className="h-5 w-5 rounded-full border-2 border-tomato9 p-1"
+            onClick={() => setChecked(true)}
+          />
+        )}
+        <div className={cn(`overflow-hidden overflow-ellipsis text-sm md:text-base text-mauve12`, checked && "text-mauve8")}>ああ</div>
+      </div>
+      {checked && (
+        <button className="w-fit whitespace-nowrap rounded-md px-2 text-tomato9 text-sm md:text-base hover:bg-mauve3">削除</button>
+      )}
     </div>
   );
 };
