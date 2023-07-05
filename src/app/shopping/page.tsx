649d55bf8d72da9841d494fc9345f333fc7f08e4
shun1121 <hikitanikun.ware80000@gmail.com>
2023-07-05 11:00:55 +0900

    :bug: 指摘事項修正反映
    指摘事項修正反映

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,17 +1,16 @@
-"use client";
-
 import { useState } from "react";
+
 import TopBar from "@/src/components/layout/top-bar";
-import { ShoppingListItem } from "@/src/components/shopping-list";
+import { EditableChecklistItem } from "@/src/components/shopping-list";
+
 import { Checkbox } from "../../components/ui/checkbox";
 import { cn } from "../../lib/utils";
 
 const page = () => {
-
   return (
     <>
-      <TopBar centerComponent={<div className="md:text-xl font-bold text-mauve12">買い物リスト</div>} />
-      <ShoppingListItem />
+      <TopBar centerComponent={<div className="font-bold text-mauve12 md:text-xl">買い物リスト</div>} />
+      <EditableChecklistItem text="ああ" />
     </>
   );
 };
--- src/components/shopping-list.tsx
+++ src/components/shopping-list.tsx
@@ -1,35 +1,46 @@
 "use client";
 
 import { useState } from "react";
+
 import { Check } from "lucide-react";
+
 import { cn } from "../lib/utils";
 
+type Props = {
+  text: string;
+};
+
 // 買い物リスト
-export const ShoppingListItem = () => {
+export const EditableChecklistItem = ({ text: name }: Props) => {
   const [checked, setChecked] = useState(false);
 
   return (
-    <div className="flex justify-between gap-x-2 border-y pl-4 pr-3 py-4">
+    <div className="flex justify-between gap-x-2 border-y py-4 pl-4 pr-3">
       <div className="flex items-center gap-2">
-        {checked ? (
-          <button
-            aria-label="Toggle checkbox"
-            className="flex h-5 w-5 items-center justify-center rounded-full border-mauve1 bg-mauve8 p-1 text-mauve1"
-            onClick={() => setChecked(false)}
-          >
-            <Check className="h-5 w-5" />
-          </button>
-        ) : (
-          <button
-            aria-label="Toggle checkbox"
-            className="h-5 w-5 rounded-full border-2 border-tomato9 p-1"
-            onClick={() => setChecked(true)}
-          />
-        )}
-        <div className={cn(`overflow-hidden overflow-ellipsis text-sm md:text-base text-mauve12`, checked && "text-mauve8")}>ああ</div>
+        <button
+          aria-label={checked ? "Uncheck item" : "Check item"}
+          className={
+            checked
+              ? "flex h-5 w-5 items-center justify-center rounded-full border-mauve1 bg-mauve8 p-1 text-mauve1"
+              : "h-5 w-5 rounded-full border-2 border-tomato9 p-1"
+          }
+          onClick={() => setChecked(!checked)}
+        >
+          {checked && <Check />}
+        </button>
+        <div
+          className={cn(
+            `overflow-hidden overflow-ellipsis text-sm text-mauve12 md:text-base`,
+            checked && "text-mauve8"
+          )}
+        >
+          {name}
+        </div>
       </div>
       {checked && (
-        <button className="w-fit whitespace-nowrap rounded-md px-2 text-tomato9 text-sm md:text-base hover:bg-mauve3">削除</button>
+        <button className="w-fit whitespace-nowrap rounded-md px-2 text-sm text-tomato9 hover:bg-mauve3 md:text-base">
+          削除
+        </button>
       )}
     </div>
   );
