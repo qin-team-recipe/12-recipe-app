6a4ddc3bd7c6496bd9263d2f644b55de838a9861
shun1121 <hikitanikun.ware80000@gmail.com>
2023-07-07 02:06:10 +0900

    :bug: リンクリスト指摘事項修正
    リンクリスト指摘事項修正

--- src/components/link-list.tsx
+++ src/components/link-list.tsx
@@ -8,26 +8,19 @@ import { cn } from "../lib/utils";
 import { Input } from "./ui/input";
 
 export const LinkListItem = () => {
-  const [, setUrl] = useState("");
   const [inputCount, setInputCount] = useState(1);
-
-  const handleUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value;
-    setUrl(input);
-  };
 
   const increaseInput = () => {
     setInputCount(inputCount + 1);
   };
 
   return (
-    <div className="">
+    <div>
       <div className="text-md my-1 ml-4 font-bold">リンク（任意）</div>
       {[...Array(inputCount)].map((_, index) => (
         <Input
-          key={index + 1}
-          className={cn(`border-x-none rounded-none`, index >= 1 && "border-t-0")}
-          onChange={handleUrl}
+          key={index}
+          className={cn(`border-x-none rounded-none`, index > 0 && "border-t-0")}
         />
       ))}
       <button className="ml-4 mt-2 flex items-center text-sm text-tomato9 md:text-base" onClick={increaseInput}>
