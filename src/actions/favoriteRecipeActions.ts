4e90b8d995a93c9217825f5f245cb43a7938fa7c
kame <133310768+hokoripon@users.noreply.github.com>
2023-07-11 08:35:01 +0900

    ✨ お気に入りレシピ追加&お気に入りレシピ削除API実装 (#94)
    * ✨ お気に入りレシピ追加&お気に入りレシピ削除API実装
    
    * 🎨 addFavoriteRecipe.tsとdeleteFavoriteRecipe.tsを1つのファイルにまとめる
    
    * 🔥 addFavoriteRecipe.tsとdeleteFavoriteRecipe.tsの削除
    
    * Merge branch 'main' into feat/92/favorite_add_delete_api

---
+++ src/actions/favoriteRecipeActions.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import { prisma } from "../lib/prisma";
+import { getAuthenticatedUser } from "./getAuthenticatedUser";
+
+export const addFavoriteRecipe = async (recipeId: number) => {
+  const user = await getAuthenticatedUser();
+
+  if (!user) {
+    throw new Error("認証に失敗しました");
+  }
+
+  await prisma.favorite.create({
+    data: {
+      userId: user.id,
+      recipeId,
+    },
+  });
+
+  // TODO: 適切なパスを指定する
+  revalidatePath("/mock");
+};
+
+export const deleteFavoriteRecipe = async (recipeId: number) => {
+  const user = await getAuthenticatedUser();
+
+  if (!user) {
+    throw new Error("認証に失敗しました");
+  }
+
+  await prisma.favorite.delete({
+    where: {
+      userId_recipeId: {
+        userId: user.id,
+        recipeId,
+      },
+    },
+  });
+
+  // TODO: 適切なパスを指定する
+  revalidatePath("/mock");
+};
---
+++ src/app/mock/kame/_components/add-favorite-recipe-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useTransition } from "react";
+
+import { addFavoriteRecipe } from "@/src/actions/favoriteRecipeActions";
+import { Button } from "@/src/components/ui/button";
+import Spinner from "@/src/components/ui/spinner";
+
+const AddFavoriteRecipeButton = ({ recipeId }: { recipeId: number }) => {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <Button onClick={() => startTransition(() => addFavoriteRecipe(recipeId))}>
+      {isPending ? <Spinner /> : "追加"}
+    </Button>
+  );
+};
+
+export default AddFavoriteRecipeButton;
