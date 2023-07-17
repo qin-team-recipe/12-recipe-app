3af2320b7f4eba14ed4dc27dd94c2832646143f9
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-07-17 22:17:45 +0900

    ♻️ レイアウト関連の見直し

---
+++ src/app/(root)/shopping/page.tsx
@@ -0,0 +1,15 @@
+import TopBar from "@/src/components/layout/top-bar";
+import { LinkListItem } from "@/src/components/link-list";
+import { EditableChecklistItem } from "@/src/components/shopping-list";
+
+const page = () => {
+  return (
+    <>
+      <TopBar centerComponent={<div className="font-bold text-mauve12 md:text-xl">買い物リスト</div>} />
+      <EditableChecklistItem text="ああ" />
+      <LinkListItem />
+    </>
+  );
+};
+
+export default page;
