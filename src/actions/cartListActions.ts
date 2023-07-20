cdc06c990075067a1bd69a6d16e4fb5a3f23d97d
hokori <hokori.pon@gmail.com>
2023-07-20 23:18:42 +0900

    ✨ お買い物追加(単数)APIの実装

---
+++ src/actions/cartListActions.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+
+import { prisma } from "../lib/prisma";
+import { Database } from "../types/SupabaseTypes";
+
+const getMaxDisplayOrder = async (userId: string) => {
+  const maxDisplayOrderRecord = await prisma.cartList.findFirst({
+    where: {
+      userId,
+    },
+    orderBy: {
+      displayOrder: "desc",
+    },
+  });
+  const isCartListEmpty = maxDisplayOrderRecord === null;
+  return isCartListEmpty ? 0 : maxDisplayOrderRecord.displayOrder + 1;
+};
+
+export const addCartList = async (recipeId: string, ingredientId: number) => {
+  const supabaseServerClient = createServerActionClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabaseServerClient.auth.getSession();
+
+  if (!session) {
+    throw new Error("認証に失敗しました");
+  }
+
+  const cartList = await prisma.cartList.findFirst({
+    where: {
+      recipeId,
+      userId: session.user.id,
+    },
+    include: {
+      CartListItem: true,
+    },
+  });
+
+  if (cartList === null) {
+    // カート内にレシピが存在しない場合は既存のお買い物リストの中で一番表示順が大きくなるようにレシピを追加する
+    const maxDisplayOrder = await getMaxDisplayOrder(session.user.id);
+
+    await prisma.cartList.create({
+      data: {
+        userId: session.user.id,
+        recipeId,
+        displayOrder: maxDisplayOrder,
+        CartListItem: {
+          create: {
+            ingredientId,
+          },
+        },
+      },
+    });
+  } else {
+    const existsIngredientId = cartList.CartListItem.find((item) => item.ingredientId === ingredientId);
+
+    if (!existsIngredientId) {
+      throw new Error("選択された材料は既にカートに追加されています。");
+    }
+
+    await prisma.cartListItem.create({
+      data: {
+        cartListId: cartList.id,
+        ingredientId,
+      },
+    });
+  }
+};
