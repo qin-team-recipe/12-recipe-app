6d847c87749e178b5bb342cc33b22aa0f625e52c
shun1121 <hikitanikun.ware80000@gmail.com>
2023-08-03 17:49:49 +0900

    :recycle: 設定ページ修正
    設定ページ修正

--- src/app/(root)/favorite/setting/page.tsx
+++ src/app/(root)/favorite/setting/page.tsx
@@ -1,29 +1,61 @@
-import HeaderArrowToGoBack from "@/src/components/header-arrow-to-go-back";
-import SettingList from "@/src/components/setting-list";
+'use client'
 
-const page = () => {
+import TopBar from "@/src/components/layout/top-bar";
+import SettingListTile from "@/src/components/setting-list";
+import { AlertCircle, ArrowLeft, ArrowUpRight, ChevronRight, LogOut } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
+
+const Page = () => {
+  const router = useRouter()
+  const handleClick = (actionType: string) => {
+    if (actionType === "terms") {
+      router.push('/terms')
+    } else if (actionType === "privacy_policy") {
+      router.push('/privacy_policy')
+    } else if (actionType === "company") {
+      window.open('https://google.com', '_blank')
+    } else if (actionType === "contact") {
+      window.open('https://youtube.com', '_blank')
+    } else if (actionType === "modal") {
+      console.log("modal")
+    }
+  }
   return (
     <>
-      <div className="relative h-16">
-        <HeaderArrowToGoBack path={"favorite/"} color="text-black" text="設定" border={true} />
-      </div>
-      <div className="my-3 ml-6 text-lg font-bold">利用規約やお問い合わせ</div>
-      <div className="mb-8">
-        <SettingList path="/" icon={<img src="/icon-chevron-right.svg" alt=""/>} text="利用規約" />
-        <SettingList path="/" icon={<img src="/icon-chevron-right.svg" alt=""/>} text="プライバシーポリシー" />
-        <SettingList path="/" icon={<img src="/icon-arrow-up-right.svg" alt=""/>} text="運営会社" external />
-        <SettingList path="/" icon={<img src="/icon-arrow-up-right.svg" alt=""/>} text="お問い合わせ" external />
-      </div>
-      <div className="my-3 ml-6 text-lg font-bold">アカウントの操作</div>
-      <div className="mb-8">
-        <SettingList path="/" icon={<img src="/icon-logout.svg" alt=""/>} text="ログアウト" />
-      </div>
-      <div className="my-3 ml-6 text-lg font-bold">取り消しができない操作</div>
-      <div className="mb-8">
-        <SettingList icon={<img src="/icon-alert-circle.svg" alt=""/>} text="退会する" />
-      </div>
+      <TopBar
+        leadingComponent={
+          <div className="flex items-center gap-3">
+            <Link href={"/favorite"}>
+              <ArrowLeft size={20} />
+            </Link>
+            <h1 className="font-bold text-mauve12 md:text-xl">設定</h1>
+          </div>
+        }
+      />
+      <section className="my-3">
+        <h2 className="ml-4 text-lg font-bold">利用規約やお問い合わせ</h2>
+        <div className="mb-8">
+          <SettingListTile onClick={() => handleClick("terms")} icon={ChevronRight} title="利用規約" />
+          <SettingListTile onClick={() => handleClick("privacy_policy")} icon={ChevronRight} title="プライバシーポリシー" />
+          <SettingListTile onClick={() => handleClick("company")} icon={ArrowUpRight} title="運営会社" />
+          <SettingListTile onClick={() => handleClick("contact")} icon={ArrowUpRight} title="お問い合わせ" />
+        </div>
+      </section>
+      <section className="my-3">
+        <h2 className="ml-4 text-lg font-bold">アカウントの操作</h2>
+        <div className="mb-8">
+          <SettingListTile onClick={() => handleClick("modal")} icon={LogOut} title="ログアウト" />
+        </div>
+      </section>
+      <section className="my-3">
+        <h2 className="ml-4 text-lg font-bold">取り消しができない操作</h2>
+        <div className="mb-8">
+          <SettingListTile onClick={() => handleClick("modal")} icon={AlertCircle} title="退会する" />
+        </div>
+      </section>  
     </>
   );
 };
 
-export default page;
+export default Page;
--- src/components/setting-list.tsx
+++ src/components/setting-list.tsx
@@ -1,83 +1,23 @@
-"use client";
-
-import { ReactNode, useState } from "react";
-import Link from "next/link";
+import { LucideIcon } from "lucide-react";
 
 type Props = {
-  path?: string;
-  text: string;
-  icon: ReactNode;
-  external?: boolean;
+  title: string;
+  icon: LucideIcon;
+  onClick?: () => void;
 };
 
-const SettingList = ({ path, text, icon, external }: Props) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-  const modalOpen = () => {
-    setIsModalOpen(true);
-  };
-  const handleDelete = () => {
-    if (inputValue === "delete") {
-      console.log("削除実行");
-      setIsModalOpen(false);
-    }
-    setInputValue("");
-  };
-
-  const modal = (
-    <div
-      className="fixed inset-0 z-20 flex items-center justify-center bg-gray-500/60 p-10"
-      onClick={() => setIsModalOpen(false)}
-    >
-      <div className="" onClick={(e) => e.stopPropagation()}>
-        <p className="mb-1 text-white">アカウントを削除するには”delete”と入力してください。</p>
-        <div className="block">
-          <input
-            type="text"
-            value={inputValue}
-            onChange={onInputChange}
-            className="w-full rounded bg-mauve4 py-2 pl-2 placeholder:text-mauve9"
-            placeholder="delete"
-          />
-          <div className="mt-2 flex justify-center">
-            <button onClick={handleDelete} className="w-16 rounded bg-red-500 py-2 text-white">
-              削除
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-
+const SettingListTile = ({ title, icon: Icon, onClick }: Props) => {
   return (
     <div>
-      {isModalOpen && modal}
-      {path ? (
-        external ? (
-          <a href={path} className="flex cursor-pointer justify-between px-5 py-2 text-mauve1" target="_blank" rel="noopener noreferrer">
-            {text && <div className="ml-1 pt-1 text-lg text-mauve12">{text}</div>}
-            {icon}
-          </a>
-        ) : (
-          <Link href={path} className={`flex cursor-pointer justify-between px-5 py-2 text-mauve1`}>
-            {text && <div className="ml-1 pt-1 text-lg text-mauve12">{text}</div>}
-            {icon}
-          </Link>
-        )
-      ) : (
-        <button
-          onClick={modalOpen}
-          className={`flex w-full cursor-pointer items-center justify-between px-5 py-2 text-mauve1`}
-        >
-          {text && <div className="ml-1 pt-1 text-lg text-mauve12">{text}</div>}
-          {icon}
-        </button>
-      )}
+      <button
+        onClick={onClick}
+        className={`flex w-full cursor-pointer items-center justify-between px-5 py-2 text-mauve1`}
+      >
+        <div className="ml-1 pt-1 text-lg text-mauve12">{title}</div>
+        <Icon className="text-mauve12" />
+      </button>
     </div>
   );
 };
 
-export default SettingList;
+export default SettingListTile;
