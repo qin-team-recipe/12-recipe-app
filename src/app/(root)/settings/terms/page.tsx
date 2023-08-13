1cc439a406876e319e2c9d7f525d81d95c4f6859
shun1121 <hikitanikun.ware80000@gmail.com>
2023-08-13 20:54:06 +0900

    :recycle: 利用規約FB修正
    利用規約FB修正

--- src/app/(root)/settings/terms/page.tsx
+++ src/app/(root)/settings/terms/page.tsx
@@ -1,6 +1,36 @@
 import Link from "next/link";
 import TopBar from "@/src/components/layout/top-bar";
 import { ArrowLeft } from "lucide-react";
+import TermsSection from "../_components/terms-section";
+
+const termsList = [
+  {
+    title: "第一項",
+    termsText: (
+      <div className="mt-4 text-[14px]">
+        <p className="mb-4">
+          吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+        </p>
+        <p>
+          吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: "第二項",
+    termsText: (
+      <div className="mt-4 text-[14px]">
+        <p className="mb-4">
+          吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+        </p>
+        <p>
+          吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+        </p>
+      </div>
+    ),
+  },
+];
 
 const page = () => {
   return (
@@ -8,27 +38,16 @@ const page = () => {
       <TopBar
         leadingComponent={
           <div className="flex items-center gap-3">
-            <Link href={"/favorite"}>
+            <Link href={"/settings"}>
               <ArrowLeft size={20} />
             </Link>
             <h1 className="font-bold text-mauve12 md:text-xl">利用規約</h1>
           </div>
         }
       />
-      <section className="px-6 py-4">
-        <h2 className="mt-1 text-lg font-bold">第一項</h2>
-        <div className="mt-4 text-[14px]">
-          <p className="mb-4">吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。</p>
-          <p>吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。</p>
-        </div>
-      </section>
-      <section className="px-6 py-4">
-        <h2 className="mt-1 text-lg font-bold">第二項</h2>
-        <div className="mt-4 text-[14px]">
-          <p className="mb-4">吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。</p>
-          <p>吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。</p>
-        </div>
-      </section>
+      {termsList.map((terms, index) => (
+        <TermsSection key={index} title={terms.title} termsText={terms.termsText} />
+      ))}
     </>
   );
 };
