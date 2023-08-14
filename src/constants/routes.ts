315f34bbf3725019a2caa2a2f775ab5dfb116f8c
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-08-14 22:59:42 +0900

    ✨ ロゴ・ファビコン・フォントの設定 & その他微修正

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -7,7 +7,7 @@ export const favoriteBasePath = "/favorite";
 export const shoppingListBasePath = "/shopping-list";
 
 export const sideBarRoutes = (pathname: string) => [
-  { href: "/", label: "話題を検索", icon: Search, active: pathname === "/" || pathname.includes(searchBasePath) },
+  { href: "/", label: "一流レシピ", icon: Search, active: pathname === "/" || pathname.includes(searchBasePath) },
   { href: favoriteBasePath, label: "お気に入り", icon: Heart, active: pathname === favoriteBasePath },
   { href: shoppingListBasePath, label: "買い物リスト", icon: ShoppingCart, active: pathname === shoppingListBasePath },
 ];
