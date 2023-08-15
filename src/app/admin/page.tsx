93ad2b03baff71d614eb1149585f129472d17707
hokori <hokori.pon@gmail.com>
2023-08-16 00:53:25 +0900

    ✨ ロールがADMIN以外のユーザがadminページに遷移した場合admin/loginに遷移リダイレクトする

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,22 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import { getChefs } from "@/src/actions/getChefs";
+import { getUserRole } from "@/src/actions/getUserRole";
+import type { Database } from "@/src/types/SupabaseTypes";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import { ChefsDataTable } from "./_components/chefs-data-table";
 
 const page = async () => {
+  const cookieStore = cookies();
+  const {
+    data: { session },
+  } = await createServerComponentClient<Database>({ cookies: () => cookieStore }).auth.getSession();
+
+  const role = await getUserRole(session?.user.id);
+  if (role === undefined || role !== "ADMIN") redirect("/admin/login");
+
   const data = await getChefs();
 
   return (
