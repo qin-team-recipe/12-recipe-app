0585194f0b8458aea6f24c3a479d0188b6b53695
shun1121 <hikitanikun.ware80000@gmail.com>
2023-09-01 19:03:36 +0900

    :recycle: レシピ詳細ページカート追加処理実装
    レシピ詳細ページカート追加処理実装

--- src/app/recipe/[id]/ingredients/page.tsx
+++ src/app/recipe/[id]/ingredients/page.tsx
@@ -3,22 +3,27 @@ import CopyIngredientsToClipboardButton from "@/src/components/copy-ingredients-
 
 import AllIngredientsToCart from "../_components/add-all-ingredients-to-cart";
 import IngredientToCartIcon from "../_components/add-ingredient-to-cart-icon";
+import { getCartList } from "@/src/actions/getCartList";
 
 const page = async ({ params }: { params: { id: string } }) => {
   const { Ingredient: ingredients, servingCount, title } = await getRecipeById(params.id);
-  const ingredientIds = ingredients.map((ingredient) => Number(ingredient.id));
+  const { isAllCartListItemCompleted, checkIngredientInCart } = await getCartList();
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex justify-between border-b p-4">
         <h2 className="text-xl font-bold">{servingCount}人前</h2>
-        <AllIngredientsToCart recipeId={ingredients[0].recipeId} ingredientIds={ingredientIds} isActive />
+        <AllIngredientsToCart
+          recipeId={ingredients[0].recipeId}
+          ingredientIds={ingredients.map((ingredient) => ingredient.id)}
+          isActive={isAllCartListItemCompleted}
+        />
       </div>
       <ul>
         {ingredients.map(({ title, id, recipeId }) => (
           <li key={id} className="flex justify-between border-b px-4 py-2">
             <p>{title}</p>
-            <IngredientToCartIcon id={id} recipeId={recipeId} isActive />
+            <IngredientToCartIcon id={id} recipeId={recipeId} isActive={checkIngredientInCart(id)} />
           </li>
         ))}
       </ul>
