f35ed0243ed5f478e1b3024f9646dec0ba8cefa6
tuboihirokidesu <tuboi.hiroki1108@gmail.com>
2023-09-04 22:46:33 +0900

    🐛

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,17 +1,26 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { getUserRole } from "@/src/actions/getUserRole";
 import { ROLE_TYPE } from "@/src/constants/constants";
 import type { Database } from "@/src/types/SupabaseTypes";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createServerComponentClient, Session } from "@supabase/auth-helpers-nextjs";
 
 import TopBar from "@/src/components/layout/top-bar";
 
-import { AdminLoginForm, LoginFormValues } from "./_components";
+import { AdminLoginForm } from "./_components";
 
 export const metadata = {
   title: "管理者ログイン画面",
+};
+
+const isUser = (session: Session | null) => {
+  const isUserRole = session?.user.user_metadata.role === ROLE_TYPE.USER;
+  const isGithubLogin = session?.user.app_metadata.provider === "github";
+  return isUserRole || isGithubLogin;
+};
+
+const isAdmin = (session: Session | null) => {
+  return session && session.user.user_metadata.role !== ROLE_TYPE.USER;
 };
 
 const AdminLoginPage = async () => {
@@ -20,10 +29,8 @@ const AdminLoginPage = async () => {
     data: { session },
   } = await createServerComponentClient<Database>({ cookies: () => cookieStore }).auth.getSession();
 
-  // ログイン済かつロールがUSERの場合はルートへリダイレクト
-  if (session && session.user.user_metadata.role === ROLE_TYPE.USER) redirect("/");
-  // ログイン済かつロールがUSERではない場合はロールがADMINと判断して/adminへリダイレクト
-  if (session && session.user.user_metadata.role !== ROLE_TYPE.USER) redirect("/admin");
+  if (isUser(session)) return redirect("/");
+  if (isAdmin(session)) return redirect("/admin");
 
   return (
     <>
