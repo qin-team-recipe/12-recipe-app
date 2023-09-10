be40c8de7c3037fd63d40e9acc9b22d8739b3928
TomoyaOgashiwa <tomoya07oga@yahoo.co.jp>
2023-09-10 17:34:22 +0900

    🐛 指摘事項の修正

--- src/prisma/seed/index.ts
+++ src/prisma/seed/index.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
 
 const prisma = new PrismaClient();
 
 const main = async () => {
   // ダミーデータ用の値なので、ここのidはレシピ関係のuser_idを満たすためだけに必要なもの
   // なので、厳密なauthenticationに必要なダミーは用意しない(SupabaseやGithubで認証が必要になるのでシードデータだと対応できない)
-  const dummyUserId = "1";
+  const dummyUserId = uuidv4();
   const dummyUser = prisma.user.create({
     data: {
       id: dummyUserId,
@@ -41,7 +42,10 @@ const main = async () => {
         },
       },
       RecipeImage: {
-        create: { recipeImage: "" },
+        create: {
+          recipeImage:
+            "https://images.unsplash.com/photo-1595295333158-4742f28fbd85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fCVFMyU4MyU5MSVFMyU4MiVCOSVFMyU4MiVCRnxlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop",
+        },
       },
     },
   });
